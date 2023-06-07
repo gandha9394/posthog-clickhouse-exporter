57098c7160977998409096ed8e1b54909e3b76b6
Gandharva Bettadapur <gandharva@setu.co>
2023-06-07 15:48:56 +0530

    Init

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 // <TODO: your plugin code here - you can base it on the code below, but you don't have to>
+import clickhouse from '@clickhouse/client';
 
 // Some internal library function
 async function getRandomNumber() {
+    const client = clickhouse.createClient({
+        host: 'clickhouse-dev.setu.co',
+        port: 443,
+        username: 'gb_test',
+        password: 'test@1234',
+        database: 'fiu_posthog_db'
+    })
+    const res = await client.query({query: "select * from events;"})
+    console.log(res)
     return 4
 }
 
 // Plugin method that runs on plugin load
 export async function setupPlugin({ config }) {
     console.log(`Setting up the plugin`)
+    const client = clickhouse.createClient({
+        host: 'clickhouse-dev.setu.co',
+        port: 443,
+        username: 'gb_test',
+        password: 'test@1234',
+        database: 'fiu_posthog_db'
+    })
+    const res = await client.query({query: "select * from events;"})
+    console.log(res)
+    return 4
 }
 
 // Plugin method that processes event
